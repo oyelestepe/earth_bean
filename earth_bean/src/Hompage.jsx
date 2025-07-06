684576cbb7e7db7c842f69f05a3eedc91b65632e
Onurcan Yelestepe <oyelestepe1@gmail.com>
2025-07-06 12:32:27 +0300

    Add cart functionality with Redux for product management

--- earth_bean/src/Hompage.jsx
+++ earth_bean/src/Hompage.jsx
@@ -1,98 +1,108 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { useSelector, useDispatch } from 'react-redux'
+import { addToCart } from './store/cartSlice'
+import { useNavigate } from 'react-router-dom'
+
 function Hompage() {
+  const products = useSelector(state => state.cart.products);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleAddToCart = (id) => {
+    dispatch(addToCart(id));
+    navigate('/cart');
+  };
+
   return (
     <div>
-        <Navbar />
-        <div className='hero-container'>
-            <div className='hero-text'>
-                <h1>Crafted from Earth, Brewed for You.</h1>
-                <h2>Sustainably sourced coffee rooted in tradition and taste.</h2>
-            </div>
-            <div className='hero-image'>
-                <img src='hero-img.png' alt='hero' />
-            </div>
-        </div>
+      <Navbar />
+      <div className='hero-container'>
+          <div className='hero-text'>
+              <h1>Crafted from Earth, Brewed for You.</h1>
+              <h2>Sustainably sourced coffee rooted in tradition and taste.</h2>
+          </div>
+          <div className='hero-image'>
+              <img src='hero-img.png' alt='hero' />
+          </div>
+      </div>
 
-    <div className='about-container'>
-  <div className='about-text'>
-    <h1><strong>Lorem ipsum dolor sit amet.</strong></h1>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consequat turpis
-      et turpis auctor, tristique. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    </p>
+  <div className='about-container'>
+<div className='about-text'>
+  <h1><strong>Lorem ipsum dolor sit amet.</strong></h1>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consequat turpis
+    et turpis auctor, tristique. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+  </p>
+</div>
+<img className="about-splash-img" src="coffee-splash2.png" alt="coffee splash" />
   </div>
-  <img className="about-splash-img" src="coffee-splash2.png" alt="coffee splash" />
-    </div>
 
-    {/* PRODUCTS */}
-        <div className='products'>
-            <h1>Our Products</h1>
-                <div className='product-wrapper'>
-                    <div className='product'>
-                        <h3>Product Name</h3>
-                        <img src='product2.png' alt='product' />
-                        <button className='product-button'>Add to Cart</button>
-                    </div>
-
-                     <div className='product'>
-                        <h3>Product Name</h3>
-                        <img src='product1.png' alt='product' />
-                        <button className='product-button'>Add to Cart</button>
-                    </div>
-
-                     <div className='product'>
-                        <h3>Product Name</h3>
-                        <img src='product3.png' alt='product' />
-                        <button className='product-button'>Add to Cart</button>
-                    </div>
-                <div/>
+  {/* PRODUCTS */}
+      <div className='products'>
+        <h1>Our Products</h1>
+        <div className='product-wrapper'>
+          {products.map(product => (
+            <div className='product' key={product.id}>
+              <h3>{product.name}</h3>
+              <img src={product.image} alt='product' />
+              <p style={{ color: "#fff", fontWeight: "bold", fontSize: "18px" }}>
+                ${product.price.toFixed(2)}
+              </p>
+              <button
+                className='product-button'
+                onClick={() => handleAddToCart(product.id)}
+              >
+                Add to Cart
+              </button>
             </div>
+          ))}
         </div>
-    {/* PRODUCTS */}
+      </div>
+  {/* PRODUCTS */}
 
-    {/* PROCCESS */}
-    <h2 className='process-title'>Our Process</h2>
-        <div className='process-wrapper'>
-            <div className='process-container'>
-                <div className='process-text'>
-                    <h3>Step 1</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut tempor enim, non dictum quam. Nulla ut vehicula risus. Phasellus non neque pretium, condimentum ante id, lobortis eros. Mauris vestibulum.</p>
-                </div>
-                <div className='process-image'>
-                    <img src='process1.jpg' alt='process' />
-                </div>
-            </div>
+  {/* PROCCESS */}
+  <h2 className='process-title'>Our Process</h2>
+      <div className='process-wrapper'>
+          <div className='process-container'>
+              <div className='process-text'>
+                  <h3>Step 1</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut tempor enim, non dictum quam. Nulla ut vehicula risus. Phasellus non neque pretium, condimentum ante id, lobortis eros. Mauris vestibulum.</p>
+              </div>
+              <div className='process-image'>
+                  <img src='process1.jpg' alt='process' />
+              </div>
+          </div>
 
-            <div className='process-container'>
-                <div className='process-text'>
-                    <h3>Step 2</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut tempor enim, non dictum quam. Nulla ut vehicula risus. Phasellus non neque pretium, condimentum ante id, lobortis eros. Mauris vestibulum.</p>
-                </div>
-                <div className='process-image'>
-                    <img src='process2.jpg' alt='process' />
-                </div>
-            </div>
+          <div className='process-container'>
+              <div className='process-text'>
+                  <h3>Step 2</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut tempor enim, non dictum quam. Nulla ut vehicula risus. Phasellus non neque pretium, condimentum ante id, lobortis eros. Mauris vestibulum.</p>
+              </div>
+              <div className='process-image'>
+                  <img src='process2.jpg' alt='process' />
+              </div>
+          </div>
 
-            <div className='process-container'>
-                <div className='process-text'>
-                    <h3>Step 3</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut tempor enim, non dictum quam. Nulla ut vehicula risus. Phasellus non neque pretium, condimentum ante id, lobortis eros. Mauris vestibulum.</p>
-                </div>
-                <div className='process-image'>
-                    <img src='process3.png' alt='process' />
-                </div>
-            </div>
-        </div>
-    {/* PROCCESS */}
+          <div className='process-container'>
+              <div className='process-text'>
+                  <h3>Step 3</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut tempor enim, non dictum quam. Nulla ut vehicula risus. Phasellus non neque pretium, condimentum ante id, lobortis eros. Mauris vestibulum.</p>
+              </div>
+              <div className='process-image'>
+                  <img src='process3.png' alt='process' />
+              </div>
+          </div>
+      </div>
+  {/* PROCCESS */}
 
-    <div className=''>
-        <h2>lorem ipsum dollar</h2>
-        <img src='coffee-cup.png' alt='coffee cup' />
-    </div>
+  <div className=''>
+      <h2>lorem ipsum dollar</h2>
+      <img src='coffee-cup.png' alt='coffee cup' />
+  </div>
 
-    </div>
-  )
+  </div>
+)
 }
 
 export default Hompage
---
+++ earth_bean/src/store/cartSlice.jsx
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  products: [
+    { id: 1, name: 'Earth Bean Classic', price: 12.99, image: 'product2.png', quantity: 0 },
+    { id: 2, name: 'Earth Bean Dark Roast', price: 13.99, image: 'product1.png', quantity: 0 },
+    { id: 3, name: 'Earth Bean Espresso', price: 14.99, image: 'product3.png', quantity: 0 },
+  ]
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const product = state.products.find(p => p.id === action.payload);
+      if (product) {
+        product.quantity += 1;
+      }
+    },
+    updateQuantity: (state, action) => {
+      const { id, delta } = action.payload;
+      const product = state.products.find(p => p.id === id);
+      if (product) {
+        product.quantity = Math.max(1, product.quantity + delta);
+      }
+    }
+  }
+});
+
+export const { addToCart, updateQuantity } = cartSlice.actions;
+export default cartSlice.reducer;
